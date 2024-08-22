c5ea7af201f581bd8c6ac572fdc3047532a00c2d
Haroon-64 <haroonn475@gmail.com>
2024-08-22 15:15:28 +0530

    awt _ swing

---
+++ java/AWT/AButton.java
@@ -0,0 +1,29 @@
+import java.awt.Button; 
+import java.applet.Applet; 
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent; 
+import java.awt.Component; 
+/*<applet code=AButton.class width=100 height=100> 
+  </applet> */ 
+ 
+public class AButton  implements ActionListener { 
+  public void init() { 
+    // STEP 1: Compose the GUI 
+    Button beepButton = new Button("Beep");  
+    add(beepButton); 
+ 
+    // STEP 2: Setup Event handlers 
+    beepButton.addActionListener(this); 
+ 
+    // STEP 3: Display the GUI (automatic -- this is an applet) 
+  } 
+public void actionPerformed(ActionEvent event) { 
+    Component c = (Component)event.getSource(); 
+   c.getToolkit().beep(); 
+  } 
+
+public static void main(String[] args) {
+    
+}
+
+} 
