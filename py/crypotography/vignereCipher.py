aa7cab8f47eea76d80c801e33b53deb5ca4f9881
haroonn <haroonn475@gmail.com>
2025-02-09 17:51:41 +0530

    uitest

--- py/crypotography/vignereCipher.py
+++ py/crypotography/vignereCipher.py
@@ -8,12 +8,25 @@ def generate_key(msg, key):
 def encrypt(msg, key):
     table, key = generate_key(msg, key)
     
-    return ''.join(table[ord(m) - 65][ord(k) - 65] for m, k in zip(msg.upper(), key))
+    cipher_text = []
+    for m, k in zip(msg.upper(), key):
+        row = ord(m) - 65
+        col = ord(k) - 65
+        cipher_text.append(table[row][col])
+
+    return ''.join(cipher_text)
 
 def decrypt(msg, key):
     table, key = generate_key(msg, key)
     
-    return ''.join(chr(table[ord(k) - 65].index(m) + 65) for m, k in zip(msg.upper(), key))
+    decoded_text = []
+    for m, k in zip(msg.upper(), key):
+        row = ord(k) - 65  
+        col = table[row].index(m)  
+        decoded_text.append(chr(col + 65)) 
+
+    return ''.join(decoded_text)
+
 
 plaintext = input("Enter plaintext: ").upper()
 plaintext = ''.join(filter(str.isalpha, plaintext)).replace('J', 'I')
@@ -23,4 +36,4 @@ ciphertext = encrypt(plaintext, keyword)
 print("Ciphertext:", ciphertext)
 
 decrypted_text = decrypt(ciphertext, keyword)
-print("Decrypted text:", decrypted_text)
+print("Decrypted text:", decrypted_text.lower())
